f3f438c4b53f9f211956df6bc5faf676a2dd738b
OpenShift guest <fxiii.cloud9@gmail.com>
2013-04-15 12:33:05 -0400

    get some time screwing ops1

--- www/js/lib/app.js
+++ www/js/lib/app.js
@@ -6,7 +6,7 @@ require(["pairing/view"],function(Pairing){
     a = new Pairing();
     a.on("gotRoom",function(p){alert("room:",p)});
     a.on("timeout",function(p){alert("timeout")});
-    Forl.on("start",function(){
+    Forlr.on("start",function(){
         $("#pairing").html(a.render().$el);
     });
 });
